fe6af0ce7642daea7e4d1d946125e6c022714001
Kirk Chu <kkchu791@gmail.com>
2026-06-03 22:49:57 -0700

    broker read and append

--- internal/app/broker/handler.go
+++ internal/app/broker/handler.go
@@ -1,11 +1,67 @@
 package broker
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/kkchu791/sounds/internal/model"
 )
 
+type Server struct {
+	broker *model.Broker
+}
+
+func NewServer(ID string, p *model.Partition) *Server {
+	return &Server{
+		broker: model.NewBroker(ID, p),
+	}
+}
+
+func (s *Server) ReadHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		offsetStr := req.URL.Query().Get("offset")
+		offset, err := strconv.Atoi(offsetStr)
+
+		if err != nil {
+			http.Error(w, "invalid offset", http.StatusBadRequest)
+			return
+		}
+
+		msg, err := s.broker.Read(offset)
+
+		if err != nil {
+			http.Error(w, "had some trouble reading", http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(msg)
+	} else {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
 // individual handler func fo each route
-func AppendHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Giving \n")
+func (s *Server) AppendHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		var message model.Message
+		err := json.NewDecoder(req.Body).Decode(&message)
+
+		if err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		s.broker.Append(&message)
+
+		w.WriteHeader(http.StatusCreated)
+		io.WriteString(w, "message appended successfully")
+	} else {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 }
--- internal/app/broker/run.go
+++ internal/app/broker/run.go
@@ -1,16 +1,21 @@
 package broker
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/kkchu791/sounds/internal/model"
 )
 
 // sets up the server
 func Run() {
-	fmt.Println("heyo")
+	log.Println("Broker Listening on port 5001")
 
-	http.HandleFunc("/append", AppendHandler)
+	// create a new server with a new partition
+	server := NewServer("broker-0", model.NewPartition())
+
+	http.HandleFunc("/append", server.AppendHandler)
+	http.HandleFunc("/read", server.ReadHandler)
 	log.Fatal(http.ListenAndServe(":5001", nil))
 
 }
