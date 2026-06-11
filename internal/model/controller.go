fde89b40628c22b1626de0c69b824c7701712506
Kirk Chu <kkchu791@gmail.com>
2026-06-11 15:51:34 -0700

    add controller model

---
+++ internal/model/controller.go
@@ -0,0 +1,26 @@
+package model
+
+import "time"
+
+type Controller struct {
+	Brokers    map[string]*BrokerInfo
+	Partitions map[string]*PartitionInfo
+}
+
+type PartitionInfo struct {
+	LeaderID string
+	ISR      []string
+	Replicas []string
+}
+
+type BrokerInfo struct {
+	Address  string
+	LastSeen time.Time
+}
+
+func NewController() *Controller {
+	return &Controller{
+		Brokers:    make(map[string]*BrokerInfo),
+		Partitions: make(map[string]*PartitionInfo),
+	}
+}
