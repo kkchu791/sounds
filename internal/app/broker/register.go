1edb08f08c468028fb565f5a7b3e0de06f1e4a66
Kirk Chu <kkchu791@gmail.com>
2026-06-13 15:24:02 -0700

    add client register call on broker

---
+++ internal/app/broker/register.go
@@ -0,0 +1,51 @@
+package broker
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type RegisterRequest struct {
+	BrokerID   string
+	BrokerAddr string
+}
+
+type RegisterResponse struct {
+	IsLeader    bool
+	LeaderAddr  string
+	PartitionID string
+}
+
+func Register(brokerID, brokerAddr, controllerAddr string) (*RegisterResponse, error) {
+	req := RegisterRequest{
+		BrokerID:   brokerID,
+		BrokerAddr: brokerAddr,
+	}
+
+	var buf bytes.Buffer //creates a Buffer object that grows automatically, implements io.Writer
+
+	err := json.NewEncoder(&buf).Encode(req)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(controllerAddr+"/register", "application/json", &buf)
+	if err != nil {
+		log.Printf("post failed: %v", err)
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	var res RegisterResponse
+	err = json.NewDecoder(resp.Body).Decode(&res)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+
+}
