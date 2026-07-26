eb701061f47e01bea15f2e68a8c4651c0023dc6a
Kirk Chu <kkchu791@gmail.com>
2026-07-26 15:52:53 -0700

    KAF-18: adding CreateTopic handler

---
+++ internal/app/controller/handler_createtopic.go
@@ -0,0 +1,64 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// request structs
+type CreateTopicsRequest struct {
+	Topics []Topic `json:"topics"`
+}
+
+type Topic struct {
+	Name              string
+	PartitionCount    int
+	ReplicationFactor int
+}
+
+// response structs
+type CreateTopicsResponse struct {
+	Results []Result `json:"results"`
+}
+
+type Result struct {
+	Name      string `json:"name"`
+	ErrorCode int    `json:"error_code"`
+	ErrorMsg  string `json:"error_msg,omitempty"`
+}
+
+func (s *Server) CreateTopicHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req CreateTopicsRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	// TODO: add logic here
+	// takes in a topic configurations like topic name partition count, replication factor
+	//result, err := s.Controller.CreateTopic()
+
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+	//just dummy day for now
+	result := Result{Name: "Sounds", ErrorCode: 0}
+
+	results := []Result{result}
+
+	res := CreateTopicsResponse{
+		Results: results,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(res)
+}
